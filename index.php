d69a84f601709a2550e2f059bb5f64c52953d5ee
excaliburnt <marshallbrown12@yahoo.com>
2020-11-05 17:41:16 -0500

    title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Do My List</title>
 </head>
 <body>
 <p>Hello world!</p>
