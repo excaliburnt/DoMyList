31fe795a93672e24720c4da057b434b94e5b4e77
excaliburnt <marshallbrown12@yahoo.com>
2020-11-05 19:51:17 -0500

    sections added

--- index.php
+++ index.php
@@ -7,13 +7,22 @@
 </head>
 <body>
 	<main>
-		<div class="add-section">
+		<section class="add-section">
 			<form action="">
-				<input type="text" name="title" placeholder="This is field is required bro">
+				<input type="text" 
+						name="title" 
+						placeholder="This is field is required bro">
+				<button type ="submit">Add &nbsp; <span>&#43;</span></button>
 			</form>
+		</section>
+		<section class="todo-section">
+			<div class="todo-item">
+			<input type="checkbox">
+				<h2>Wassup</h2>
+				<br>
+				<small>Created by Marshmallow</small>
 		</div>
+		</section>
 	</main>
-
 </body>
-
 </html>
