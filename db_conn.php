39442289debfcc1d1948e9b1a04418489fc33a3d
excaliburnt <marshallbrown12@yahoo.com>
2020-11-07 19:46:49 -0500

    More php

--- db_conn.php
+++ db_conn.php
@@ -6,10 +6,10 @@
  $db_name ="domylist";
 
  try{
-	$conn = new PDO("mysql:host=$sName;db_name=$db_name",
+	$conn = new PDO("mysql:host=$sName;dbname=$db_name",
 					$uName, $pass);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "You're connected dude!";
+
  }catch(PDOException $e){
    echo "Conection failed! : ". $e->getMessage();
  }
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require 'db_conn.php'
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -13,16 +16,27 @@
 				<input type="text" 
 						name="title" 
 						placeholder="Enter a task">
-				<button type ="submit">Add &nbsp; <span>&#43;</span></button>
+				<button type ="submit">Add &nbsp; <span>&#43; </span></button>
 			</form>
 		</section>
+		<?php
+			$domylist = $conn ->query("SELECT * FROM domylist ORDER BY id DESC");
+		?>
 		<section class="todo-section">
+		<?php if($domylist->rowCount() <= 0){ ?>
 			<div class="todo-item">
-			<input type="checkbox">
+				<div class="empty">
+					<h1>Add a task bro!</h1>
+				</div>
+			</div>
+		<?php }?>
+		
+			<div class="todo-item">
+			    <input type="checkbox">
 				<h2>Workout</h2>
 				<br>
 				<small>11/6/2020</small>
-		</div>
+			</div>
 		</section>
 	</main>
 </body>
